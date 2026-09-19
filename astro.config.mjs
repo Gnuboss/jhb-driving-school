018bb7887298071b64c938960d78d9d316888187
Bronson Harrington <gnuboss@gmail.com>
2026-09-18 18:24:48 -0600

    Update astro.config.mjs
    updated to use custom domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gnuboss.github.io",
-  base: "/jhb-driving-school",
+  site: "https://jhbdrivingschool.co.za", // Update to your real live domain
+  base: "/",                              // Change from repository name to root path
   devToolbar: {
     enabled: false
   },
